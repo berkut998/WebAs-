cdad247c3b4cb7489b7cf2f5b08b6581bee2f58d
Dmitry <berkut_998@mail.ru>
2021-07-19 21:54:57 +0300

    Initial commit

---
+++ qmlcache_loader.cpp
@@ -0,0 +1,99 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _0x5f_CustomControls_Text_materialForm_ui_0x2e_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f_CustomControls_SearchBoxForm_ui_0x2e_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f_CustomControls_Material_RoundButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f_CustomControls_Material_Button_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f_CustomControls_SearchBox_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f_CustomControls_Text_material_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/Text_materialForm.ui.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_Text_materialForm_ui_0x2e_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/SearchBoxForm.ui.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_SearchBoxForm_ui_0x2e_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/Material_RoundButton.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_Material_RoundButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/Material_Button.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_Material_Button_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/SearchBox.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_SearchBox_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/CustomControls/Text_material.qml"), &QmlCacheGeneratedCode::_0x5f_CustomControls_Text_material_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    Q_CLEANUP_RESOURCE(qml_qmlcache);
+    return 1;
+}
